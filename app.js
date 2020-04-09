2754ffec597e8ba1a6abee19c55dc450c351ba60
David Salami <wizdave97@gmail.com>
2020-04-09 23:32:05 +0100

    fixed error in route naming

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
-app.use('/api/on-covid-19', indexRouter);
-app.use('/api/on-covid-19/logs', logsRouter);
+app.use('/api/v1/on-covid-19', indexRouter);
+app.use('/api/v1/on-covid-19/logs', logsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
